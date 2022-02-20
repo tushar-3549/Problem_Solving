a0f0854a1531d1dbd973153e2fb8949a5812911c
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-02-20 12:37:17 +0600

    Create Avoid Contact.cpp

---
+++ Codechef/Avoid Contact.cpp
@@ -0,0 +1,78 @@
+                          //  Md Tushar Ahmed 
+                          // From : Dhaka , Bangladesh
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+#define tushar  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+#define sq(x)   (x)*(x)
+#define PI      acos(-1.0)
+#define endl      '\n'
+#define mod 1000000007
+typedef long long int ll;
+// FACTORIAL CHECK
+int fact(int n) 
+{
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+// PRIME CHECK
+bool isPrime(int num) 
+{
+    bool flag = true;
+    for (int i = 2; i <= sqrt(num); i++) 
+    {
+        if (num % i == 0) 
+        {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+// MAIN FUNCTION 
+int main()
+{
+    tushar;
+    
+    // SOLUTION
+    int t;
+    cin >> t;
+    while(t--)
+    {
+      int x,y;
+      cin >> x >> y;
+      if(x == y)
+      {
+        cout << 2*x-1 << endl;
+      }
+      else
+      {
+        cout << x + y << endl;
+      }
+    }
+    return 0;
+}
