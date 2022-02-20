315a9fdc69bc8ef133041a7453e33a603b9b6828
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-02-20 12:43:56 +0600

    Create Can You Eat it.cpp

---
+++ Codechef/Can You Eat it.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while( t-- )
+    {
+        int n,k;
+        cin >> n>> k;
+        if(n % k == 0)
+        {
+            cout << n/k <<endl;
+        }
+        else
+        {
+            cout << -1 <<endl;
+        }
+    }
+
+    return 0;
+}
+
