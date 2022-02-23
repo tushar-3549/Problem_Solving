daaa155c09dad7ef074a33585776dafa86aa1918
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-02-23 16:16:45 +0600

    Create Life, the universe and Everything.cpp

---
+++ SPOJ Online Judge/Life, the universe and Everything.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,t;
+while(1){
+ 
+scanf("%d",&a);
+if(a==42)
+{
+    break;
+}
+else
+{
+   printf("%d\n",a);
+}
+}
+return 0;
+}
