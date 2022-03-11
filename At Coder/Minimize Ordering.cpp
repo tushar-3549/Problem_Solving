65b002d731c186adfe113f35ddb913945732c315
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-03-11 20:18:51 +0600

    Create Minimize Ordering.cpp

---
+++ At Coder/Minimize Ordering.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ 
+#define Tushar_Ahmed  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long int ll;
+
+int main()
+{
+    Tushar_Ahmed; // FAST INPUT OUTPUT
+    
+    // SOLUTION
+    string s;
+    cin >> s;
+    sort(s.begin(),s.end());
+    cout << s << endl;
+    
+    return 0;
+}
