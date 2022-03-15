041fb54f45f556aff955ed9247ea46ac5a4c207d
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-03-15 20:07:24 +0600

    Create Erasing zeros.cpp

---
+++ Codeforces/Erasing zeros.cpp
@@ -0,0 +1,85 @@
+/*                               بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم 
+                         Author:   Md Tushar Ahmed 
+                           From : Dhaka , Bangladesh
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+#define Tushar_Ahmed  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+#define sq(x)         (x)*(x)
+#define PI            acos(-1.0)
+#define endl          '\n'
+#define mod           1000000007
+#define gcd(x,y)      __gcd(x,y)
+#define lcm(x,y)      (x/gcd(x,y))*y 
+
+typedef long long int ll;
+const int MAX_N = 1e5 + 5;
+const ll INF = 1e9;
+// FACTORIAL CHECK
+int fact(int n) 
+{
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+// PRIME CHECK
+bool isPrime(int num) 
+{
+    bool flag = true;
+    for (int i = 2; i <= sqrt(num); i++) 
+    {
+        if (num % i == 0) 
+        {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+// MAIN FUNCTION 
+
+int main()
+{
+    Tushar_Ahmed; // FAST INPUT OUTPUT
+    
+   // SOLUTION
+    int t;
+    cin >> t;
+    while(t--) {
+        string s, x;
+        cin >> s;
+        int pos1 = 0, pos2 = 0, cnt = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '1') {
+                pos1 = i;
+                break;
+            }
+        }
+        for(int i = s.size(); i >= pos1; i--) {
+            if(s[i] == '1') {
+                pos2 = i;
+                break;
+            }
+            /* else {
+                x += '1';
+            } */
+        }
+        for(int i = pos1; i < pos2; i ++) {
+            if(s[i] == '0') {
+                cnt ++;
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
