0d6f5f1161c8ab78a58549862d3d82d30d56c93f
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-04-10 22:15:40 +0600

    Create 1665A - GCD vs LCM.cpp

---
+++ Codechef/1665A - GCD vs LCM.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int t;
+     cin >> t;
+     while(t--)
+     {
+          int n;
+          cin >> n;
+          cout << n-3 << " " << 1 << " " << 1 << " "<< 1 << endl;
+     }
+     return 0;
+}
