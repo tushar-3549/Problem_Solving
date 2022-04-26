e5a05238da01bc999c336d9e5300c3db4d61772b
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-04-26 15:35:09 +0600

    Create 1672B - I love AAAB.cpp

---
+++ Codeforces/1672B - I love AAAB.cpp
@@ -0,0 +1,84 @@
+/*                                               بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم 
+                                                Author:   Md Tushar Ahmed 
+                                                From : Dhaka , Bangladesh
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+#define Tushar_Ahmed  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+#define sq(x)         (x)*(x)
+#define PI            acos(-1.0)
+#define endl          '\n'
+#define mod           1000000007
+#define gcd(x,y)      __gcd(x,y)
+#define lcm(x,y)      (x/gcd(x,y))*y 
+
+typedef long long int ll;
+const int MAX_N = 1e5 + 5;
+const ll INF = 1e9;
+// FACTORIAL CHECK
+int fact(int n) 
+{
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+// PRIME CHECK
+bool isPrime(int num) 
+{
+    bool flag = true;
+    for (int i = 2; i <= sqrt(num); i++) 
+    {
+        if (num % i == 0) 
+        {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+// MAIN FUNCTION 
+
+int main()
+{
+    Tushar_Ahmed; // FAST INPUT OUTPUT
+    
+   // SOLUTION
+    int t;
+    cin >> t;
+    while( t-- ) {
+        string s;
+        cin >> s;
+        if(s.size() == 1 or s[0] == 'B' or s[s.size()-1] == 'A') {
+          cout << "NO\n";
+          continue;
+        }
+        int flag = 0;
+        int carry = 0;
+        for(int i = 0; i < s.size(); i++){
+           if(s[i] == 'A') carry += 1;
+            else if(s[i] == 'B') carry -= 1;
+            if(carry < 0) {
+              flag = 1;
+              break;
+            }
+        }
+        if(flag == 0) {
+          cout << "YES\n";
+        }
+        else {
+           cout << "NO\n";
+        }
+    }
+    return 0;
+}
