0252b62faee6da679be195b5ee9994797e291660
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-05-07 20:37:53 +0600

    Create Roman to Integer.cpp

---
+++ LeetCode/Roman to Integer.cpp
@@ -0,0 +1,19 @@
+// Easy
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        map < char, int > mp = {{'I',1}, {'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int n = s.size();
+        int total = mp[s.back()];
+        for(int i = n-2; i>=0; i--) {
+            if(mp[i] < mp[i+1]) {
+                total -= mp[s[i]];
+            }
+            else {
+                total += mp[s[i]];
+            }
+        }
+        return total;
+    }
+};
