61f029b6d95091a512586a9ea57499ffd3a09998
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-05-11 11:14:52 +0600

    Create 1676C - Most Similar Words	.cpp

---
+++ Codeforces/1676C - Most Similar Words	.cpp
@@ -0,0 +1,94 @@
+/*                               بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم 
+                                 Author:   Md Tushar Ahmed 
+                                 From : Dhaka , Bangladesh
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define Tushar_Ahmed  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+#define    sq(x)         (x)*(x)
+#define    PI            acos(-1.0)
+#define    endl          '\n'
+#define    mod           1000000007
+#define    gcd(x,y)      __gcd(x,y)
+#define    lcm(x,y)      (x/gcd(x,y))*y 
+#define    ll            long long int
+#define    ull           unsigned long long int
+#define    ui            unsigned int
+#define    INF           LLONG_MAX
+#define    modd          998244353
+
+// FACTORIAL CHECK
+int fact(int n) 
+{
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+// PRIME CHECK
+bool isPrime(int num) 
+{
+    bool flag = true;
+    for (int i = 2; i <= sqrt(num); i++) 
+    {
+        if (num % i == 0) 
+        {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+// MAIN FUNCTION 
+//int minii = INT_MAX;
+int call(string x, string y) {
+    int res = 0;
+    int n = x.size();
+    for(int i = 0; i < n; i++) {
+        res += abs(x[i] - y[i]);
+    }
+    return res;
+}
+int main()
+{
+    Tushar_Ahmed; // FAST INPUT OUTPUT
+    
+   // SOLUTION
+    
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+        //string s[n+1][m+1];
+        string s[n+1];
+        int minii = INT_MAX;
+        /* for(int i = 0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                cin >> s[i][j];
+            }
+        } */
+        for(int i=0;i<n;i++) {
+            cin >> s[i];
+        }
+        
+        for(int i=0; i < n; i++) {
+            for(int j=i+1; j < n; j++) {
+                minii = min(minii,call(s[i],s[j]));
+        }
+     }
+     cout << minii << endl; 
+
+    }
+    return 0;
+}
+
