d8c2bba9a454d1391c57b5d9ed39f7bc9cb3180e
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-06-30 16:16:16 +0600

    Create Generate Parenthesis.cpp

---
+++ LeetCode/Generate Parenthesis.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector <string> valid;
+void generate(string &s, int open, int close)
+{
+    if(open == 0 and close == 0)
+    {
+        valid.push_back(s);
+        return;
+    }
+    if(open > 0)
+    {
+        s.push_back('(');
+        generate(s,open-1,close);
+        s.pop_back();
+    }
+    if(close > 0)
+    {
+        if(open < close)
+        {
+            s.push_back(')');
+            generate(s,open,close-1);
+            s.pop_back();
+        }
+    }
+}
+    vector<string> generateParenthesis(int n) {
+        string s;
+        generate(s,n,n);
+        return valid;
+        
+    }
+};
