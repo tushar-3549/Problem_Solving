4522adbf42cbbf1fba036c5017049b9f142c3f03
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-07-25 19:58:47 +0600

    Update Cf Round 886.cpp

--- Codeforces/Cf Round 886.cpp
+++ Codeforces/Cf Round 886.cpp
@@ -35,6 +35,28 @@ void solveC()
 	}
 	cout << endl;
 }
+void solveD()
+{
+	int n, k;
+	cin >> n >> k;
+	int arr[n+1];
+	for(int i = 0; i < n; i++)
+		cin >> arr[i];
+	int ans = 1, cnt = 1;
+	sort(arr, arr+n);
+	for(int i = 1; i < n; i++){
+		if(arr[i] - arr[i-1] <= k){
+			cnt++;
+		}
+		else{
+			ans = max(ans, cnt);
+			cnt = 1;
+		}
+	}
+	ans = max(ans, cnt);
+
+	cout << n-ans << endl;
+}
 int main()
 {
     int t;
@@ -43,7 +65,8 @@ int main()
     {
         //solveA();
 		//solveB();
-		solveC();
+		// solveC();
+		solveD();
     }
 	return 0;
 }
